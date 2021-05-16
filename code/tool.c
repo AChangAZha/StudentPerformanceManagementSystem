2e2fde63f34ef665ffbffe51ed47258c7c59418c
ZhangHan <202041404130@dgut.edu.cn>
2021-05-16 11:01:55 +0800

    add sort

--- code/tool.c
+++ code/tool.c
@@ -205,7 +205,7 @@ void about()
 {
     printf("----------------------------------------------------\n");
     printf("                  学生成绩管理系统                 \n");
-    printf("                 version 1.3.1 Alpha                  \n");
+    printf("                 version 1.4.0 Alpha                  \n");
     printf("                       2021/5                     \n");
     printf("                    东莞理工学院                   \n");
     printf("                  网络空间安全学院                 \n");
